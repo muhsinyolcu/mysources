e1d2925c0471534e36a60672fa89e3c39f4b7d88
Muhsin Yolcu <muhsin.yolcu@outlook.com>
2020-05-02 15:15:06 +0300

    Export a data to Excel method 1

--- mySource.WebApp.MVC/Controllers/ProductController.cs
+++ mySource.WebApp.MVC/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using mySource.WebApp.MVC.Services.Product;
+using System.IO;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace mySource.WebApp.MVC.Controllers
 {
@@ -13,6 +16,27 @@ namespace mySource.WebApp.MVC.Controllers
         public ActionResult Index()
         {
             return View(_productService.GetProducts());
+        }
+        public ActionResult ExportToExcelMethod1()
+        {
+            var products = _productService.GetProducts();
+
+            var gV = new GridView();
+            gV.DataSource = products;
+            gV.DataBind();
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=DemoExcel.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            StringWriter objStringWriter = new StringWriter();
+            HtmlTextWriter objHtmlTextWriter = new HtmlTextWriter(objStringWriter);
+            gV.RenderControl(objHtmlTextWriter);
+            Response.Output.Write(objStringWriter.ToString());
+            Response.Flush();
+            Response.End();
+
+            return View("Index", products);
         }
     }
 }
