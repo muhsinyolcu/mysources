fd0e3226ab6ff4d1bca7e9dd1e3422e30ff58c3d
Muhsin Yolcu <muhsin.yolcu@outlook.com>
2020-05-02 15:21:10 +0300

    Export a data to Excel method 2 with ClosedXML

--- mySource.WebApp.MVC/Controllers/ProductController.cs
+++ mySource.WebApp.MVC/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using mySource.WebApp.MVC.Services.Product;
+﻿using ClosedXML.Excel;
+using mySource.WebApp.MVC.Services.Product;
+using System.Data;
 using System.IO;
 using System.Web.Mvc;
 using System.Web.UI;
@@ -13,10 +15,12 @@ namespace mySource.WebApp.MVC.Controllers
         {
             _productService = productService;
         }
+
         public ActionResult Index()
         {
             return View(_productService.GetProducts());
         }
+
         public ActionResult ExportToExcelMethod1()
         {
             var products = _productService.GetProducts();
@@ -37,6 +41,33 @@ namespace mySource.WebApp.MVC.Controllers
             Response.End();
 
             return View("Index", products);
+        }
+
+        public FileResult ExportToExcelMethod2()
+        {
+            var products = _productService.GetProducts();
+
+            DataTable dt = new DataTable("Products");
+            dt.Columns.AddRange(new DataColumn[3] {
+            new DataColumn("Id"),
+            new DataColumn("Product Name"),
+            new DataColumn("Price")
+            });
+
+            foreach (var product in products)
+            {
+                dt.Rows.Add(product.ProductId, product.ProductName, product.ProductPrice);
+            }
+
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                wb.Worksheets.Add(dt);
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    wb.SaveAs(stream);
+                    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Products.xlsx");
+                }
+            }
         }
     }
 }
